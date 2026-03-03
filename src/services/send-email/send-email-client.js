53a2834e1a76dd041e18274381cebde6c6084b10
Jon Wyatt <jon.wyatt@equalexperts.com>
2026-03-03 14:17:35 +0000

    NRF2-478 - Send email utility (#35)
    * Send email utility
    
    * Notify config
    
    * Don't specify email variables in sendEmail
    
    * Use createLogger instead of request logger
    
    * Don't pass logger into sendEmail from tests
    
    * Get notification ID from data property on notify response

---
+++ src/services/send-email/send-email-client.js
@@ -0,0 +1,43 @@
+import { createNotifyClient } from './notify-client.js'
+import { createLogger } from '../../common/helpers/logging/logger.js'
+
+export const sendEmail = async ({
+  recipientEmailAddress,
+  emailReference,
+  emailBodyVariables,
+  templateId
+}) => {
+  const logger = createLogger()
+  const notifyClient = createNotifyClient()
+  const options = {
+    personalisation: emailBodyVariables,
+    reference: emailReference
+  }
+  try {
+    const result = await notifyClient.sendEmail(
+      templateId,
+      recipientEmailAddress,
+      options
+    )
+    const notificationId = result?.data?.id
+    if (!notificationId) {
+      throw new Error('No notification ID returned')
+    }
+    logger.info('Notify sendEmail responded', {
+      templateId,
+      notificationId
+    })
+    return {
+      notificationId,
+      sentDateTime: new Date().toISOString()
+    }
+  } catch (error) {
+    logger.error(
+      {
+        templateId
+      },
+      `Notify sendEmail failed: ${error.message}`
+    )
+    return null
+  }
+}
---
+++ src/services/send-email/send-email-client.test.js
@@ -0,0 +1,112 @@
+import { sendEmail } from './send-email-client.js'
+import { createNotifyClient } from './notify-client.js'
+import { createLogger } from '../../common/helpers/logging/logger.js'
+vi.mock('./notify-client.js')
+vi.mock('../../common/helpers/logging/logger.js')
+
+describe('sendEmail', () => {
+  let notifySendEmail
+  let logger
+  const recipientEmailAddress = 'barry@builders.com'
+  const estimateReference = 'AN2191'
+  const templateId = 'test-template-id'
+  const emailBodyVariables = {
+    estimateReference,
+    levyAmount: '£3500',
+    monitoringAmount: '£250',
+    maintenanceAmount: '£560',
+    adminAmount: '£80'
+  }
+  const notificationId = '201b576e-c09b-467b-9dfa-9c3b689ee730'
+
+  beforeEach(() => {
+    notifySendEmail = vi.fn().mockResolvedValue({
+      data: { id: notificationId }
+    })
+    logger = {
+      info: vi.fn(),
+      error: vi.fn()
+    }
+    vi.mocked(createLogger).mockReturnValue(logger)
+    vi.mocked(createNotifyClient).mockReturnValue({
+      sendEmail: notifySendEmail
+    })
+  })
+
+  it('calls the notify sendEmail function', async () => {
+    await sendEmail({
+      recipientEmailAddress,
+      emailReference: estimateReference,
+      emailBodyVariables,
+      templateId
+    })
+    expect(notifySendEmail).toHaveBeenCalledWith(
+      templateId,
+      recipientEmailAddress,
+      {
+        personalisation: {
+          adminAmount: '£80',
+          estimateReference: 'AN2191',
+          levyAmount: '£3500',
+          maintenanceAmount: '£560',
+          monitoringAmount: '£250'
+        },
+        reference: 'AN2191'
+      }
+    )
+  })
+
+  it('returns a sent date & time and the notification ID, and logs, if successful', async () => {
+    const result = await sendEmail({
+      recipientEmailAddress,
+      emailReference: estimateReference,
+      emailBodyVariables,
+      templateId
+    })
+    expect(result).toEqual({
+      notificationId,
+      sentDateTime: expect.any(String)
+    })
+    expect(logger.info).toHaveBeenCalledWith('Notify sendEmail responded', {
+      templateId,
+      notificationId
+    })
+  })
+
+  it('returns null and logs, if unsuccessful', async () => {
+    const error = new Error('Notify sendEmail failed')
+    notifySendEmail.mockRejectedValue(error)
+    const result = await sendEmail({
+      recipientEmailAddress,
+      emailReference: estimateReference,
+      emailBodyVariables,
+      templateId
+    })
+    expect(result).toBeNull()
+    expect(logger.error).toHaveBeenCalledWith(
+      {
+        templateId
+      },
+      `Notify sendEmail failed: ${error.message}`
+    )
+  })
+
+  it("returns null and logs, if a notification ID isn't returned", async () => {
+    notifySendEmail.mockResolvedValue({
+      id: null
+    })
+    const result = await sendEmail({
+      recipientEmailAddress,
+      emailReference: estimateReference,
+      emailBodyVariables,
+      templateId
+    })
+    expect(result).toBeNull()
+    expect(logger.error).toHaveBeenCalledWith(
+      {
+        templateId
+      },
+      `Notify sendEmail failed: No notification ID returned`
+    )
+  })
+})
