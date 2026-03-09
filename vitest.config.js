9feb9bb534ec6a658a607729321a97f03bf41fdd
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-03-09 09:59:52 +0000

    exclude int-tests

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     clearMocks: true,
+    exclude: [...configDefaults.exclude, 'int-tests'],
     env: {
       DB_PORT: '5433'
     },
