003299ebba79cd6066a7b960718cd7ccb05b4ac1
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-03-09 10:21:43 +0000

    Move checkCdpUploaderHealth

--- src/common/helpers/start-server.js
+++ src/common/helpers/start-server.js
@@ -1,7 +1,7 @@
 import { config } from '../../config.js'
 
 import { createServer } from '../../server.js'
-import { getCdpUploaderUrl } from '../../services/cdp-uploader/cdp-uploader.js'
+import { checkCdpUploaderHealth } from './cdp-uploader-health.js'
 
 async function startServer() {
   const server = await createServer()
@@ -15,26 +15,6 @@ async function startServer() {
   await checkCdpUploaderHealth(server.logger)
 
   return server
-}
-
-async function checkCdpUploaderHealth(logger) {
-  const baseUrl = getCdpUploaderUrl()
-  const url = `${baseUrl}/health`
-
-  try {
-    const response = await fetch(url)
-    if (response.ok) {
-      logger.info(`CDP Uploader health check passed - url: ${url}`)
-    } else {
-      logger.error(
-        `CDP Uploader health check failed - url: ${url}, status: ${response.status}`
-      )
-    }
-  } catch (error) {
-    logger.error(
-      `CDP Uploader health check error - url: ${url}, message: ${error.message}`
-    )
-  }
 }
 
 export { startServer }
