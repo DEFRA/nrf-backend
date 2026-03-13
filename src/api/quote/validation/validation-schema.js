52b0bb4129ce0309a2a4da43b81f916e6b293cd1
Jon Wyatt <jon.wyatt@equalexperts.com>
2026-03-13 09:07:06 +0000

    Accept all quote data at POST /quotes endpoint (#55)

---
+++ src/api/quote/validation/validation-schema.js
@@ -0,0 +1,29 @@
+import joi from 'joi'
+
+const MAX_RESIDENTIAL_UNITS = 999999
+const MAX_EMAIL_LENGTH = 254
+
+export const quoteSchema = joi.object({
+  boundaryEntryType: joi.string().valid('draw', 'upload').required(),
+  developmentTypes: joi
+    .array()
+    .items(joi.string().valid('housing', 'other-residential'))
+    .required(),
+  residentialBuildingCount: joi.when('developmentTypes', {
+    is: joi.array().has(joi.valid('housing')),
+    then: joi.number().integer().min(1).max(MAX_RESIDENTIAL_UNITS).required(),
+    otherwise: joi.any().forbidden()
+  }),
+  peopleCount: joi.when('developmentTypes', {
+    is: joi.array().has(joi.valid('other-residential')),
+    then: joi.number().integer().min(1).required(),
+    otherwise: joi.any().forbidden()
+  }),
+  email: joi
+    .string()
+    .trim()
+    .max(MAX_EMAIL_LENGTH)
+    .pattern(/^\S*$/)
+    .email({ tlds: { allow: false } })
+    .required()
+})
--- src/services/db/quotes/queries.js
+++ src/services/db/quotes/queries.js
@@ -1,7 +1,22 @@
-export const dbCreateQuote = async ({ db, emailAddress }) => {
+export const dbCreateQuote = async ({ db, quoteData }) => {
+  const {
+    boundaryEntryType,
+    developmentTypes,
+    residentialBuildingCount,
+    peopleCount,
+    email
+  } = quoteData
   const { rows } = await db.query(
-    'INSERT INTO quotes (email_address) VALUES ($1) RETURNING id, reference',
-    [emailAddress]
+    `INSERT INTO quotes (email_address, boundary_entry_type, development_types, residential_building_count, people_count)
+     VALUES ($1, $2, $3, $4, $5)
+     RETURNING id, reference`,
+    [
+      email,
+      boundaryEntryType,
+      developmentTypes,
+      residentialBuildingCount ?? null,
+      peopleCount ?? null
+    ]
   )
   return rows[0]
 }
