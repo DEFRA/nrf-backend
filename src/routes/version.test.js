4526a0e57532316e9fbe04be750a4e5326468caa
robin-dunn <58361313+robin-dunn@users.noreply.github.com>
2026-03-23 22:05:53 +0000

    Coverage

---
+++ src/routes/version.test.js
@@ -0,0 +1,18 @@
+import { statusCodes } from '../common/constants/status-codes.js'
+import { setupTestServer } from '../test-utils/setup-test-server.js'
+
+describe('GET /version', () => {
+  const getServer = setupTestServer()
+
+  it('should return the version', async () => {
+    const response = await getServer().inject({
+      method: 'GET',
+      url: '/version'
+    })
+
+    expect(response.statusCode).toBe(statusCodes.ok)
+    const body = JSON.parse(response.payload)
+    expect(body).toHaveProperty('version')
+    expect(typeof body.version).toBe('string')
+  })
+})
