230148e2e2d89d7a154f9dd79669a8b6b99353ad
Jon Wyatt <jon.wyatt@equalexperts.com>
2026-08-06 12:44:35 +0100

    Send planning type to email in lower case (#258)

--- src/api/quote/helpers/get-planning-type-display.js
+++ src/api/quote/helpers/get-planning-type-display.js
@@ -1,7 +1,7 @@
 const planningTypeDisplayNames = {
-  'full-planning-permission': 'Full planning permission',
-  'outline-planning-permission': 'Outline planning permission',
-  'hybrid-planning-permission': 'Hybrid planning permission'
+  'full-planning-permission': 'full planning permission',
+  'outline-planning-permission': 'outline planning permission',
+  'hybrid-planning-permission': 'hybrid planning permission'
 }
 
 export const getPlanningTypeDisplay = (planningType) =>
--- src/api/quote/helpers/get-planning-type-display.test.js
+++ src/api/quote/helpers/get-planning-type-display.test.js
@@ -2,9 +2,9 @@ import { getPlanningTypeDisplay } from './get-planning-type-display.js'
 
 describe('getPlanningTypeDisplay', () => {
   it.each([
-    ['full-planning-permission', 'Full planning permission'],
-    ['outline-planning-permission', 'Outline planning permission'],
-    ['hybrid-planning-permission', 'Hybrid planning permission']
+    ['full-planning-permission', 'full planning permission'],
+    ['outline-planning-permission', 'outline planning permission'],
+    ['hybrid-planning-permission', 'hybrid planning permission']
   ])('maps %s to %s', (planningType, expected) => {
     expect(getPlanningTypeDisplay(planningType)).toBe(expected)
   })
